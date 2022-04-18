39b7f4e6105d9cd0782f285692c2d1d52237dbca
Haji Bamsi <hibamsi@gmail.com>
2022-04-18 20:13:50 +0300

    add function to display books list

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 let booksList = [];
 let form = document.getElementById("add-form");
+let bookList = document.querySelector(".book-list");
 
 function addForm() {
   let title = document.getElementById("title");
   let author = document.getElementById("author");
-  let booksList = JSON.parse(window.localStorage.getItem('books'));
+  // let books = JSON.parse(window.localStorage.getItem("books"));
+  // if (books == null) {
+  //   booksList = books;
+  // }
   booksList.push({ title: title.value, author: author.value });
-  window.localStorage.setItem('books', JSON.stringify(booksList));
-  console.log(booksList);
+  window.localStorage.setItem("books", JSON.stringify(booksList));
 }
 
 form.addEventListener("submit", addForm);
 
+window.onload = function () {
+  let books = JSON.parse(window.localStorage.getItem("books"));
+  if (books != null) {
+    booksList = books;
+  }
+  let htmlList = "";
+  for (let i = 0; i < booksList.length; i += 1) {
+    htmlList += `<div>
+                      <p>${booksList[i].title}</p>
+                      <p>${booksList[i].author}</p>
+                      <button type="submit" onClick="removeBook(${i})">Remove</button>
+                      <hr />
+                    </div>`;
+  }
+  bookList.innerHTML = htmlList;
+};
