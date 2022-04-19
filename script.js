538c2030b2d1e85ee472122dbe44970c2cd5d764
HammadSiddique <hammad.siddique01@gmail.com>
2022-04-19 17:59:44 +0500

    Add book class

--- script.js
+++ script.js
@@ -1,39 +1,34 @@
-let booksList = [];
+class Book {
+  constructor (title,author){
+    this.title = title;
+    this.author = author;
+  }
+
+  getBooks() {
+    const books = JSON.parse(window.localStorage.getItem('books'));
+    return books;
+  }
+
+  addBook(){
+    
+  }
+
+  saveBook() {
+    let booksList = [];
+    let books = this.getBooks();
+    if (books != null) {
+      booksList = books;
+    }
+    booksList.push({ title: this.title, author: this.author });
+    window.localStorage.setItem('books', JSON.stringify(booksList));
+  }
+}
+
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+
 const form = document.getElementById('add-form');
-const bookList = document.querySelector('.book-list');
 
-function addForm() {
-  const title = document.getElementById('title');
-  const author = document.getElementById('author');
-  booksList.push({ title: title.value, author: author.value });
-  window.localStorage.setItem('books', JSON.stringify(booksList));
-}
-
-form.addEventListener('submit', addForm);
-
-function loadData() {
-  const books = JSON.parse(window.localStorage.getItem('books'));
-  if (books != null) {
-    booksList = books;
-  }
-  let htmlList = '';
-  for (let i = 0; i < booksList.length; i += 1) {
-    htmlList += `<div>
-                    <p>${booksList[i].title}</p>
-                    <p>${booksList[i].author}</p>
-                    <button type="submit" onClick="removeBook(${i})">Remove</button>
-                    <hr />
-                  </div>`;
-  }
-  bookList.innerHTML = htmlList;
-}
-
-window.onload = loadData();
-
-/* eslint-disable */
-function removeBook(index) {
-  const newBookList = booksList.filter((_, i) => i !== index);
-  window.localStorage.setItem("books", JSON.stringify(newBookList));
-  window.onload = loadData();
-}
-/* eslint-enable */
+form.addEventListener('submit', function() {
+  let newBook = new Book (title.value, author.value)
+});
