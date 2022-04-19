2b95e0d76f61ed963d403d94ab068850553ce508
Haji Bamsi <hibamsi@gmail.com>
2022-04-19 16:11:48 +0300

    add display books function

--- script.js
+++ script.js
@@ -1,34 +1,48 @@
 class Book {
-  constructor (title,author){
+  constructor(title, author) {
     this.title = title;
     this.author = author;
   }
 
   getBooks() {
-    const books = JSON.parse(window.localStorage.getItem('books'));
-    return books;
-  }
-
-  addBook(){
-    
+    let bookList = [];
+    const books = JSON.parse(window.localStorage.getItem("books"));
+    if (books != null) {
+      bookList = books;
+    }
+    return bookList;
   }
 
   saveBook() {
-    let booksList = [];
-    let books = this.getBooks();
-    if (books != null) {
-      booksList = books;
-    }
+    let booksList = this.getBooks();
     booksList.push({ title: this.title, author: this.author });
-    window.localStorage.setItem('books', JSON.stringify(booksList));
+    window.localStorage.setItem("books", JSON.stringify(booksList));
+  }
+
+  displayBooks() {
+    const bookList = document.querySelector(".book-list");
+    let booksList = this.getBooks();
+    let htmlList = "";
+    for (let i = 0; i < booksList.length; i += 1) {
+      htmlList += `<div>
+                    <p>${booksList[i].title}</p>
+                    <p>${booksList[i].author}</p>
+                    <button type="submit" onClick="removeBook(${i})">Remove</button>
+                    <hr />
+                  </div>`;
+    }
+    bookList.innerHTML = htmlList;
   }
 }
 
-const title = document.getElementById('title');
-const author = document.getElementById('author');
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const form = document.getElementById("add-form");
 
-const form = document.getElementById('add-form');
-
-form.addEventListener('submit', function() {
-  let newBook = new Book (title.value, author.value)
+form.addEventListener("submit", function () {
+  let newBook = new Book(title.value, author.value);
+  newBook.saveBook();
 });
+
+let book = new Book();
+window.onload = book.displayBooks();
