8c412078159b70bd0e122a7dda905d1bfdedc1d8
Haji Bamsi <hibamsi@gmail.com>
2022-04-19 11:50:11 +0300

    add readme file and fix linters

--- script.js
+++ script.js
@@ -1,41 +1,39 @@
 let booksList = [];
-let form = document.getElementById("add-form");
-let bookList = document.querySelector(".book-list");
+const form = document.getElementById('add-form');
+const bookList = document.querySelector('.book-list');
 
 function addForm() {
-  let title = document.getElementById("title");
-  let author = document.getElementById("author");
+  const title = document.getElementById('title');
+  const author = document.getElementById('author');
   booksList.push({ title: title.value, author: author.value });
-  window.localStorage.setItem("books", JSON.stringify(booksList));
+  window.localStorage.setItem('books', JSON.stringify(booksList));
 }
 
-form.addEventListener("submit", addForm);
+form.addEventListener('submit', addForm);
 
-window.onload = loadData();
-
-function loadData () {
-  let books = JSON.parse(window.localStorage.getItem("books"));
+function loadData() {
+  const books = JSON.parse(window.localStorage.getItem('books'));
   if (books != null) {
     booksList = books;
   }
-  let htmlList = "";
+  let htmlList = '';
   for (let i = 0; i < booksList.length; i += 1) {
     htmlList += `<div>
-                      <p>${booksList[i].title}</p>
-                      <p>${booksList[i].author}</p>
-                      <button type="submit" onClick= 'removeBook(${i})'>Remove</button>
-                      <hr />
-                    </div>`;
+                    <p>${booksList[i].title}</p>
+                    <p>${booksList[i].author}</p>
+                    <button type="submit" onClick="removeBook(${i})">Remove</button>
+                    <hr />
+                  </div>`;
   }
   bookList.innerHTML = htmlList;
-};
+}
 
-function removeBook (index) {
-  let newBookList = booksList.filter((_,i) => {
-    i != index
-  });
-  console.log(newBookList);
+window.onload = loadData();
+
+/* eslint-disable */
+function removeBook(index) {
+  const newBookList = booksList.filter((_, i) => i !== index);
   window.localStorage.setItem("books", JSON.stringify(newBookList));
   window.onload = loadData();
-};
-
+}
+/* eslint-enable */
